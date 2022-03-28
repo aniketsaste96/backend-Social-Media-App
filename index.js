b29a94046113d426ba9e516cc75fa7d8196cc0b9
Aniket <aniketsaste96@gmail.com>
2022-03-28 09:31:14 +0530

    cors

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
+const cors = require("cors");
 const dbConnection = require("./db.js")
 const port = process.env.PORT || 9000;
 const usersRoute = require("./routes/usersRoute")
 const postRoute = require("./routes/postsRoute")
 //even if i entered values in body getting validation error
 const bodyParser = require("body-parser")
-
+app.use(cors())
 //***middleware parses incoming requests with JSON payloads 
 //too much file upload set file size limit
 app.use(express.json({ limit: '25mb' }));
